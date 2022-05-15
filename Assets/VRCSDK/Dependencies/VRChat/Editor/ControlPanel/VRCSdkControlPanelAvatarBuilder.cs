9928e546d5e5a27a96b0e4115b3eb5f3ed5cb192
Lilithe Bowman <lilithe.bowman@gmail.com>
2022-05-15 11:10:40 -0400

    Uploading Lil Chubbo for Windows

--- Assets/VRCSDK/Dependencies/VRChat/Editor/ControlPanel/VRCSdkControlPanelAvatarBuilder.cs
+++ Assets/VRCSDK/Dependencies/VRChat/Editor/ControlPanel/VRCSdkControlPanelAvatarBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if !VRC_CLIENT
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -6,6 +7,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 using VRC.SDKBase.Editor.BuildPipeline;
+using VRC.SDKBase.Editor.Validation;
 using VRC.SDKBase.Validation.Performance;
 using VRC.SDKBase.Validation.Performance.Stats;
 using Object = UnityEngine.Object;
@@ -216,16 +218,17 @@ namespace VRC.SDKBase.Editor
         public virtual void OnGUIAvatarCheck(VRC_AvatarDescriptor avatar)
         {
             string vrcFilePath = UnityWebRequest.UnEscapeURL(EditorPrefs.GetString("currentBuildingAssetBundlePath"));
+            bool isMobilePlatform = ValidationEditorHelpers.IsMobilePlatform();
             if (!string.IsNullOrEmpty(vrcFilePath) &&
-                ValidationHelpers.CheckIfAssetBundleFileTooLarge(ContentType.Avatar, vrcFilePath, out int fileSize))
+                ValidationHelpers.CheckIfAssetBundleFileTooLarge(ContentType.Avatar, vrcFilePath, out int fileSize, isMobilePlatform))
             {
                 _builder.OnGUIWarning(avatar,
-                    ValidationHelpers.GetAssetBundleOverSizeLimitMessageSDKWarning(ContentType.Avatar, fileSize),
+                    ValidationHelpers.GetAssetBundleOverSizeLimitMessageSDKWarning(ContentType.Avatar, fileSize, isMobilePlatform),
                     delegate { Selection.activeObject = avatar.gameObject; }, null);
             }
 
-            AvatarPerformanceStats perfStats = new AvatarPerformanceStats();
-            AvatarPerformance.CalculatePerformanceStats(avatar.Name, avatar.gameObject, perfStats);
+            AvatarPerformanceStats perfStats = new AvatarPerformanceStats(ValidationEditorHelpers.IsMobilePlatform());
+            AvatarPerformance.CalculatePerformanceStats(avatar.Name, avatar.gameObject, perfStats, isMobilePlatform);
 
             OnGUIPerformanceInfo(avatar, perfStats, AvatarPerformanceCategory.Overall,
                 GetAvatarSubSelectAction(avatar, typeof(VRC_AvatarDescriptor)), null);
@@ -318,36 +321,44 @@ namespace VRC.SDKBase.Editor
             ValidateFeatures(avatar, anim, perfStats);
 
             Core.PipelineManager pm = avatar.GetComponent<Core.PipelineManager>();
-#if UNITY_ANDROID
+
             PerformanceRating rating = perfStats.GetPerformanceRatingForCategory(AvatarPerformanceCategory.Overall);
             if (_builder.NoGuiErrors())
             {
                 if (!anim.isHuman)
                 {
                     if (pm != null) pm.fallbackStatus = Core.PipelineManager.FallbackStatus.InvalidRig;
-                    _builder.OnGUIInformation(avatar, "This avatar does not have a humanoid rig so can not be used for a cross-platform fallback.");
+                    _builder.OnGUIInformation(avatar, "This avatar does not have a humanoid rig, so it can not be used as a custom fallback.");
                 }
                 else if (rating > PerformanceRating.Good)
                 {
                     if (pm != null) pm.fallbackStatus = Core.PipelineManager.FallbackStatus.InvalidPerformance;
-                    _builder.OnGUIInformation(avatar, "This avatar does not have an overall rating of Good or better so can not be used for a cross-platform fallback. See the link below for details on Avatar Optimization.");
+                    _builder.OnGUIInformation(avatar, "This avatar does not have an overall rating of Good or better, so it can not be used as a custom fallback. See the link below for details on Avatar Optimization.");
                 }
                 else
                 {
                     if (pm != null) pm.fallbackStatus = Core.PipelineManager.FallbackStatus.Valid;
-                    _builder.OnGUIInformation(avatar, "This avatar can be used as a custom cross-platform fallback.");
+                    _builder.OnGUIInformation(avatar, "This avatar can be used as a custom fallback. This avatar must be uploaded for every supported platform to be valid for fallback selection.");
                     if (perfStats.animatorCount.HasValue && perfStats.animatorCount.Value > 1)
-                        _builder.OnGUIInformation(avatar, "This avatar uses additional animators, they will be disabled on the fallback.");
+                        _builder.OnGUIInformation(avatar, "This avatar uses additional animators, they will be disabled when used as a fallback.");
                 }
+
+                // additional messages for Poor and Very Poor Avatars
+#if UNITY_ANDROID
+                if (rating > PerformanceRating.Poor)
+                    _builder.OnGUIInformation(avatar, "This avatar will be blocked by default due to performance. Your fallback will be shown instead.");
+                else if (rating > PerformanceRating.Medium)
+                    _builder.OnGUIInformation(avatar, "Other users may choose to block this avatar due to performance. Your fallback will be shown instead.");
+#else
+                if (rating > PerformanceRating.Medium)
+                    _builder.OnGUIInformation(avatar, "Other users may choose to block this avatar due to performance. Your fallback will be shown instead.");
+#endif
             }
             else
             {
                 // shouldn't matter because we can't hit upload button
                 if (pm != null) pm.fallbackStatus = Core.PipelineManager.FallbackStatus.InvalidPlatform;
             }
-#else
-            if (pm != null) pm.fallbackStatus = Core.PipelineManager.FallbackStatus.InvalidPlatform;
-#endif
         }
 
         public virtual void ValidateFeatures(VRC_AvatarDescriptor avatar, Animator anim, AvatarPerformanceStats perfStats)
@@ -594,7 +605,9 @@ namespace VRC.SDKBase.Editor
             bool hasHead;
             bool hasFeet;
             bool hasHands;
+#if VRC_SDK_VRCSDK2
             bool hasThreeFingers;
+#endif
             bool correctSpineHierarchy;
             bool correctLeftArmHierarchy;
             bool correctRightArmHierarchy;
@@ -1142,3 +1155,4 @@ namespace VRC.SDKBase.Editor
         }
     }
 }
+#endif
